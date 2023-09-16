899137249e6d6326c335c0760a0be9c82e5da9d1
Pedro Xavier <pedro.xavier@injunior.com.br>
2023-09-16 09:22:48 -0300

    feat: Finished script

--- trabalho_bd3_corrigido.py
+++ trabalho_bd3_corrigido.py
@@ -1,11 +1,10 @@
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql import *
 
-yearDf = None
-
 def import_data():
+    yearDf = None
     for i in range(1, 13):
         path = f"files/yellow_tripdata_2022-{i:02}.parquet"
         monthDf = spark.read.parquet(path).withColumn("Month", lit(i))
@@ -13,53 +12,158 @@ def import_data():
         if yearDf == None:
             yearDf = monthDf
         else:
-            yearDf.union(monthDf)
+            yearDf = yearDf.union(monthDf)
 
     return yearDf
 
+
 def clean_data(df: DataFrame):
-    pass
+    # Filtrando viagens com método de pagamento inválido
+    df = df.where("Payment_type > 0").where("Payment_type <= 6")
 
-def ganhos_mes_milha(df_original: DataFrame):
-    df_sum: DataFrame = df_original.where("Trip_distance > 0").groupBy("VendorId", "Month").sum("Total_amount", "Trip_distance")\
-        .withColumnRenamed("sum(Total_amount)", "GainsPerVendor")\
-        .withColumnRenamed("sum(Trip_distance)", "DistanceTraveled")
-
-    df_sum = df_sum.withColumn(
-        "AverageGains", col("GainsPerVendor")/col("DistanceTraveled"))
+    # Atribuir 0 ao Total_ammount se o tipo de pagamento for igual a 3(no charge)
+    df = df.withColumn("Total_amount", when(
+        col("Payment_type") == 3, lit(0)).otherwise(col("Total_amount")))
     
-    df_sum.select("VendorId", "Month", "AverageGains").orderBy(
-    'VendorId', 'Month', ascending=True).show()
+    # Filtrando as corridas em que o ponto de partida for menor que o de chegada (Ponto de partida antes do de chegada)
+    df = df.where("tpep_pickup_datetime <= tpep_dropoff_datetime")
+
+    # Removendo viagens com custo menor que 0
+    df = df.where("Total_amount >= 0.0")
+
+    # Removendo viagens com distância menor que 0
+    df = df.where("Trip_distance >= 0.0")
+
+    return df
+
+def earnings_vendor_month(df: DataFrame):
+
+    # Filtra pela distância percorrida maior que 0, agrupa por mês e agência e faz a soma do Total_amount e da Trip_distance
+    df_result = df.where("Trip_distance > 0")
+    df_result = df_result.groupBy("VendorId", "Month")
+    df_result = df_result.sum("Total_amount", "Trip_distance")\
+        .withColumnRenamed("sum(Total_amount)", "EarningsVendor")\
+        .withColumnRenamed("sum(Trip_distance)", "TotalDistance")
+    
+    # Adiciona a coluna de média entre EarningsVendor e TotalDistance
+    df_result = df_result.withColumn(
+        "AverageEarnings", col("EarningsVendor")/col("TotalDistance"))
+
+    return df_result
+
+def canceled_trips_month(df_earns: DataFrame, df: DataFrame):
+    # Calcular média geral
+    total_avg = df_earns.groupBy().avg("AverageEarnings")
+
+    # Calcular média anual por VendorId
+    df_earns = df_earns.groupBy("VendorId").avg(
+        "AverageEarnings").withColumnRenamed("avg(AverageEarnings)", "AnualAvg")
+
+    # Calcular média geral anual e adicionar coluna
+    df_total_avg = df_earns.withColumn("TotalAvg", lit(
+        total_avg.first()[0])).select(['VendorId', 'AnualAvg', 'TotalAvg'])
+
+    # Excluindo vendors que possuem a média menor que a média geral
+    df_total_avg = df.join(df_total_avg.where(
+        col('AnualAvg') > col('TotalAvg')), "VendorId")
+
+    # Remover viagens não canceladas e fazer agrupamento por mês
+    df_canceled: DataFrame = df_total_avg.filter((df_total_avg.tpep_pickup_datetime == df_total_avg.tpep_dropoff_datetime)
+                                                      & (df_total_avg.trip_distance == 0)
+                                                      & (df_total_avg.PULocationID == df_total_avg.DOLocationID))\
+        .groupBy('VendorId', 'Month').count().withColumnRenamed('count', 'CanceledTrips')
+
+    return df_canceled
 
 
-def canceled_trips_month(df_sum: DataFrame, original_df: DataFrame):
-    # GroupBy vazio para pegar media geral
-    overall_avg = df_sum.groupBy().avg("AverageGains")
+def top_ten_month(df: DataFrame):
 
-    # GroupBy para pegar media anual por VendorId
-    df_sum = df_sum.groupBy("VendorId").avg(
-        "AverageGains").withColumnRenamed("avg(AverageGains)", "AnualAvgPerVendor")
+    # Faz o agrupamento pelo VendorID e pelo Mês, somando as distâncias e o total ganho
+    df_top_dist = df.groupBy("VendorId", "Month").sum("Total_amount", "Trip_distance")\
+        .withColumnRenamed("sum(Total_amount)", "EarningsVendor")\
+        .withColumnRenamed("sum(Trip_distance)", "TotalDistance")
 
-    # Adicionar coluna de media geral anual
-    df_with_avg = df_sum.withColumn("TotalAvg", lit(
-        overall_avg.first()[0])).select(['VendorId', 'AnualAvgPerVendor', 'TotalAvg'])
+    # Adicona a coluna AverageEarnings calculando a média entre EarningsVendor e TotalDistance, ordenando em ordem crescente
+    # pelo mês, e decrescente pela média
 
-    # Join para manter apenas dados de Vendors que possuem media maior que a media geral
-    df_with_avg = original_df.join(df_with_avg.where(
-        col('AnualAvgPerVendor') > col('TotalAvg')), "VendorId")
+    df_top_earn = df_top_dist.withColumn("AverageEarnings", col(
+        "EarningsVendor")/col("TotalDistance")).orderBy(asc("Month"), desc("AverageEarnings"))
 
-    # Filtrar apenas as viagens canceladas e agrupar por mês
-    df_canceled_trips: DataFrame = df_with_avg.filter((df_with_avg.tpep_pickup_datetime == df_with_avg.tpep_dropoff_datetime)
-                                                      & (df_with_avg.Trip_distance == 0)
-                                                      & (df_with_avg.PULocationID == df_with_avg.DOLocationID))\
-        .groupBy('VendorId', 'Month').count().withColumnRenamed('count', 'CanceledTripsPerMonth')
-
-    return df_canceled_trips
+    return df_top_earn
 
 
 
-
-#MAIN
+#=================================== PROGRAMA PRINCIPAL ===================================
 
 sc = SparkContext("local", "Trabalho em Grupo")
 spark = SparkSession.builder.getOrCreate()
+
+df = import_data()
+df = clean_data(df)
+
+
+# A: Ganho médio por milha por mês por VendorId
+
+print("\nGanho médio por milha por mês:\n")
+
+df_earns = earnings_vendor_month(df)
+
+df_earns.select("VendorId", "Month", "AverageEarnings").orderBy(
+    'VendorId', 'Month', ascending=True).show()
+
+
+# B: Taxa de corridas canceladas por mês para os taxistas que possuem o ganho médio por milha superior a média geral
+ 
+print("\nTaxa de corridas canceladas por mês:\n")
+
+df_canceled_trips = canceled_trips_month(df_earns, df)
+
+df_canceled_trips.show()
+
+
+# C: Ganho médio dos top 10 taxistas que mais rodaram por mes no ano de 2022
+
+print("\nGanho médio dos top 10 por mes:\n")
+
+df_top_10_month = top_ten_month(df)
+
+df_top_10_month.select("VendorId", "Month", "AverageEarnings").show()
+
+
+
+#Reduzir 2% nos valores de viagens pagas com cartão de crédito e refazer as consultas anteriores
+
+print("=============================================================================================\n")
+print("\nConsultas considerando uma taxa de 2% no valor de viagens pagas com cartão de crédito:\n")
+print("=============================================================================================\n")
+
+df_with_tax = df.withColumn("Total_amount", when(col("Payment_type") == 1, round(col(
+    "Total_amount") * 0.98, 2)).otherwise(col("Total_amount")))
+
+
+# A: Ganho médio por milha por mês por VendorId
+
+print("\nGanho médio por milha por mês:\n")
+
+df_earns2 = earnings_vendor_month(df_with_tax)
+
+df_earns2.select("VendorId", "Month", "AverageEarnings").orderBy(
+    'VendorId', 'Month', ascending=True).show()
+
+
+# B: Taxa de corridas canceladas por mês para os taxistas que possuem o ganho médio por milha superior a média geral
+
+print("\nTaxa de corridas canceladas por mês:\n")
+
+df_canceled_trips2 = canceled_trips_month(df_earns2, df_with_tax)
+
+df_canceled_trips2.show()
+
+
+# C: Ganho médio dos top 10 taxistas que mais rodaram por mes no ano de 2022
+
+print("\nGanho médio dos top 10 por mes:\n")
+
+df_top_10_month2 = top_ten_month(df_with_tax)
+
+df_top_10_month2.select("VendorId", "Month", "AverageEarnings").show()
